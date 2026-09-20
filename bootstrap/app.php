3d23b50fa44ae0105561f13efc370b46cd674d68
Elmer Miguel <elmermiguel201931295@cunoc.edu.gt>
2026-09-20 04:48:12 -0600

    chore(deps): instalar spatie/laravel-permission y configurar middlewares y migraciones

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ use App\Http\Middleware\EnsureUserIsActive;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'activo' => EnsureUserIsActive::class,
             'password.cambiado' => EnsurePasswordChanged::class,
+            'role' => RoleMiddleware::class,
+            'permission' => PermissionMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
